9912bf1ef421bc90fa50a4bf29d78de13efa8dd9
krabhis <abhishekkr212402@gmail.com>
2025-02-02 00:01:19 +0530

    completed creat course flow and fixes course slice bug

--- Client/LMSfrontend/src/App.jsx
+++ Client/LMSfrontend/src/App.jsx
@@ -12,31 +12,39 @@ import CourseList from './Pages/Course/CourseList';
 import CourseDescription from './Pages/Course/CourseDescription';
 import Contact from './Pages/Contact';
 import Denied from './Pages/Denied';
+import CreateCourse from './Pages/Course/CreateCourse';
+import RequireAuth from './Components/Auth/RequireAuth';
+import Profile from './Pages/User/Profile';
 
 
 function App() {
 
   return (
     <>
-     <Routes>
-        <Route path="/" element={<HomePage />} ></Route>
-        <Route path="/about" element={<AboutUs />} ></Route>
-        <Route path="/contact" element={<Contact />} ></Route>
-        <Route path="/denied" element={<Denied />} ></Route>
-
-        <Route path="/signup" element={<Signup />} ></Route>
-        <Route path="/login" element={<Login />} ></Route>
-        <Route path='/courses' element={<CourseList />} />
-        <Route path='/course/description' element={<CourseDescription />} />
-
-
-
-        <Route path="*" element={<NotFound />} ></Route>
-       
-      </Routes>
+  <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/about" element={<AboutUs />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/denied" element={<Denied />} />
     
-    
-    </>
+    <Route path="/signup" element={<Signup />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/courses" element={<CourseList />} />
+    <Route path="/course/description" element={<CourseDescription />} />
+
+    {/* Protected Route for ADMIN */}
+    <Route element={<RequireAuth allowedRoles={["ADMIN"]} />}>
+      <Route path="/course/create" element={<CreateCourse />} />
+    </Route>
+
+    <Route element={<RequireAuth allowedRoles={["ADMIN","USER"]} />}>
+    <Route path='/user/profile' element={<Profile />} />
+    </Route>
+
+    {/* Catch-all 404 route */}
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+</>
    
   )
 }
